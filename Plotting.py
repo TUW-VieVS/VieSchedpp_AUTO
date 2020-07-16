11c382d4366c6417127f8c8ef4712017714ecf31
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2020-07-16 14:29:04 +0200

    bugfix colorbar scaling

--- Plotting.py
+++ Plotting.py
@@ -119,9 +119,11 @@ def polar_plots(skd, output, attribute_name):
         fig.subplots_adjust(left=0.05, right=0.95, bottom=0.15, top=0.95, wspace=0.2, hspace=0.25)
         cbar_ax = fig.add_axes([0.05, 0.07, 0.9, 0.025])
 
-    cb = fig.colorbar(h[0], cax=cbar_ax, orientation="horizontal")
+    fig.colorbar(h[0], cax=cbar_ax, orientation="horizontal")
+    vmin = min([o.duration for o in all_obs])
+    vmax = max([o.duration for o in all_obs])
     for this_h in h:
-        this_h.set_clim(cb.vmin, cb.vmax)
+        this_h.set_clim(vmin, vmax)
 
     if attribute_name == "duration":
         cbar_ax.set_xlabel("duration [sec]")
