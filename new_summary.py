40d83ec2a673f1fa507532e62c4ae458f1d6f86e
Matthias Schartner <mschartner@ethz.ch>
2021-05-01 17:12:00 +0200

    add stations column to new summary format

--- new_summary.py
+++ new_summary.py
@@ -4,7 +4,7 @@ import shutil
 import pandas as pd
 
 def start(path_main):
-    shutil.copy(path_main / "summary.txt", path_main / "summary.txt.old_format")
+    shutil.copy(path_main / "summary.txt", path_main / "summary.txt.old_format_5")
     ls = []
     names = []
     for p in path_main.iterdir():
@@ -25,7 +25,21 @@ def start(path_main):
                     break
 
         df = pd.read_csv(p / "merged_statistics.csv")
-        l = df.loc[df["version"] == v,:]
+        l = df.loc[df["version"] == v,:].copy()
+
+        # number of observations per baseline
+        tlcs = set()
+        filter_col = [col for col in l if col.startswith('n_bl_obs_')]
+        for col in filter_col:
+            name = col.split("_")[-1]
+            tlc1 = name[0:2]
+            tlc2 = name[3:5]
+            tlcs.add(tlc1)
+            tlcs.add(tlc2)
+
+        tlcs = "".join(tlcs)
+        l["stations"] = tlcs
+
         ls.append(l)
         names.append(p.parent.stem)
 
