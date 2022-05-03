677dc302234d9619d0035216d93dece68d8f16a3
Matthias Schartner <mschartner@ethz.ch>
2022-05-03 08:46:50 +0200

    finished skd to skd parser based on template

--- util/skd2skd_template.py
+++ util/skd2skd_template.py
@@ -4,15 +4,31 @@ import shutil
 
 
 def skd2skd_template(path_source, path_template):
+    """
+    This function can be used to copy some blocks of a .skd schedule to a .skd template. This is useful if you have a
+    working .skd file (the template - e.g. an old successful session) and want to add the new schedule into this
+    template. In particular, it will exchange parts of the $EXPER and $PARA block, as well as the full $SOURCES,
+    $FLUX and $SKED blocks. The original schedule file will be overwritten by this function, but a backup is generated
+    in the same folder named "{path_source}.skd.old"
+
+    Parameters
+    ----------
+    path_source path to input schedule
+    path_template path to schedule template
+
+    Returns
+    -------
+    None
+    """
+
     # some regex to find the blocks
     # things to consider: This can be the last block (no more $).
     # Additionally, there can be trailing whitespaces before the $ sign.
-    # Therefore, use (\$|\Z) as the end.
-    # Within the sources block, there might be $ signs.
-    # Therefore, use ^\s*(\$|\Z) as the end
-    sked = re.compile(f'(\$SKED[\S\s]*?)^\s*(\$|\Z)', re.MULTILINE)
-    source = re.compile(f'(\$SOURCES[\S\s]*?)^\s*(\$|\Z)', re.MULTILINE)
-    flux = re.compile(f'(\$FLUX[\S\s]*?)^\s*(\$|\Z)', re.MULTILINE)
+    # Within the sources block, there might be other $ signs.
+    # Therefore, use "^\s*\$|\Z" as the end
+    sked = re.compile(f'\$SKED[\S\s]*?(?=^\s*\$|\Z)', re.MULTILINE)
+    source = re.compile(f'\$SOURCES[\S\s]*?(?=^\s*\$|\Z)', re.MULTILINE)
+    flux = re.compile(f'\$FLUX[\S\s]*?(?=^\s*\$|\Z)', re.MULTILINE)
 
     # read template
     with open(path_template) as f:
@@ -24,9 +40,9 @@ def skd2skd_template(path_source, path_template):
     # read the schedule
     with open(path_source) as f:
         skd = f.read()
-    skd_sked = sked.findall(skd)[0][0]
-    skd_source = source.findall(skd)[0][0]
-    skd_flux = flux.findall(skd)[0][0]
+    skd_sked = sked.search(skd).group()
+    skd_source = source.search(skd).group()
+    skd_flux = flux.search(skd).group()
 
     # exchange $SOURCES, $SKED and $FLUX block of template with the blocks from the schedule
     template = re.sub(sked, skd_sked, template)
@@ -61,9 +77,9 @@ def skd2skd_template(path_source, path_template):
 
 
 if __name__ == "__main__":
-    doc = "Replaces some elements in the $EXPER block, as well as the full $SOURCES, $FLUX and $SKED block in the " \
-          "template with the values from the source. The original skd file will get renamed to session.skd.old." \
-          "The merged schedule is stored in the source location."
+    doc = "Replaces some elements in the $EXPER and $PARA block, as well as the full $SOURCES, $FLUX and $SKED " \
+          "blocks in the template with the values from the source. The original skd file will get renamed to " \
+          "session.skd.old. The merged schedule is stored in the source location."
 
     parser = argparse.ArgumentParser(description=doc)
     parser.add_argument("-s", "--source", help="path to input skd file")
